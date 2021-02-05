7bafcf5fa3d0db3f80d41a0ae02f9c801cc0dcbd
Matioma <Artiom.Matievschi@gmail.com>
2021-02-05 16:24:56 +0100

    ButtonChange heleper

---
+++ SoftwareArchitectureShop/Assets/Scripts/ButtonChangeHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonChangeHelper : MonoBehaviour
+{
+    [SerializeField]
+    Button[] CategoryButtons;
+
+    public void HighLightElement(Button pButton)
+    {
+        foreach (var button in CategoryButtons)
+        {
+            if (pButton == button)
+            {
+                button.interactable = false;
+                continue;
+            }
+            button.interactable = true;
+        }
+    }
+}
--- SoftwareArchitectureShop/Assets/Scripts/MyShop/Models/ShopState/BuyState.cs
+++ SoftwareArchitectureShop/Assets/Scripts/MyShop/Models/ShopState/BuyState.cs
@@ -19,7 +19,6 @@ public class BuyState : IShopActions
             Debug.Log("Item could not be purchased");
             return;
         }
-        Debug.Log("Buy");
         Item previousItem = shopModel.SelectLastItem();
         shopModel.shopInventory.TransferItem(previousItem, shopModel.playerInventory);
     }
